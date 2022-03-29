6b10a6786c273adf0afcba3a1a45499fb04b5e17
n4to4 <n4to4k@gmail.com>
2022-03-29 19:12:54 +0900

    Draw a Sketch

--- getting-started/src/main.rs
+++ getting-started/src/main.rs
@@ -1,15 +1,11 @@
 use nannou::prelude::*;
 
-struct Model {}
-
 fn main() {
-    nannou::app(model).event(event).simple_window(view).run();
+    nannou::sketch(view).run();
 }
 
-fn model(_app: &App) -> Model {
-    Model {}
+fn view(app: &App, frame: Frame) {
+    let draw = app.draw();
+    draw.background().color(BLUE);
+    draw.to_frame(app, &frame).unwrap();
 }
-
-fn event(_app: &App, _model: &mut Model, _event: Event) {}
-
-fn view(_app: &App, _model: &Model, _frame: Frame) {}
