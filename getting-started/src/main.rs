f0f60e1b774cf4b4d8363fa856c1ae47b5c714af
n4to4 <n4to4k@gmail.com>
2022-03-30 23:47:17 +0900

    padding

--- getting-started/src/main.rs
+++ getting-started/src/main.rs
@@ -14,8 +14,12 @@ fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
 fn view(app: &App, _model: &Model, frame: Frame) {
     let win = app.window_rect();
-    let r = Rect::from_w_h(100.0f32, 100.0f32).top_left_of(win);
+    let win_p = win.pad(25.0);
+    let r = Rect::from_w_h(100.0f32, 100.0f32).top_left_of(win_p);
     let draw = app.draw();
-    draw.rect().xy(r.xy()).wh(r.wh()).color(PLUM);
+    draw.rect()
+        .xy(r.xy())
+        .wh(r.wh())
+        .color(rgba(0.3, 0.4, 0.7, 0.5));
     draw.to_frame(app, &frame).unwrap();
 }
