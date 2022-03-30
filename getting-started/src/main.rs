005babee734ad10c5c6c1a422c2b59725c911f4f
n4to4 <n4to4k@gmail.com>
2022-03-30 23:52:37 +0900

    padding

--- getting-started/src/main.rs
+++ getting-started/src/main.rs
@@ -13,13 +13,15 @@ fn model(_app: &App) -> Model {
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
 fn view(app: &App, _model: &Model, frame: Frame) {
+    let pad = 25.0;
     let win = app.window_rect();
-    let win_p = win.pad(25.0);
-    let square = Rect::from_w_h(100.0, 100.0).top_left_of(win_p);
-
+    let win_p = win.pad(pad);
     let draw = app.draw();
+
+    let square = Rect::from_w_h(100.0, 100.0).top_left_of(win_p);
     draw.rect().xy(square.xy()).wh(square.wh()).color(PLUM);
-    let circle = square.below(square);
+
+    let circle = square.below(square).shift_y(-pad);
     draw.ellipse().xy(circle.xy()).wh(circle.wh()).color(SALMON);
 
     draw.to_frame(app, &frame).unwrap();
