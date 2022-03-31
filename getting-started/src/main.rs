62c5a025e6a60109f465288a02d9f55df72589fa
n4to4 <n4to4k@gmail.com>
2022-03-31 19:02:51 +0900

    Drawing 2D Shapes

--- getting-started/src/main.rs
+++ getting-started/src/main.rs
@@ -1,28 +1,15 @@
 use nannou::prelude::*;
 
 fn main() {
-    nannou::app(model).update(update).simple_window(view).run();
+    nannou::sketch(view).run();
 }
 
-struct Model {}
-
-fn model(_app: &App) -> Model {
-    Model {}
-}
-
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
-
-fn view(app: &App, _model: &Model, frame: Frame) {
-    let pad = 25.0;
-    let win = app.window_rect();
-    let win_p = win.pad(pad);
+fn view(app: &App, frame: Frame) {
     let draw = app.draw();
 
-    let square = Rect::from_w_h(100.0, 100.0).top_left_of(win_p);
-    draw.rect().xy(square.xy()).wh(square.wh()).color(PLUM);
+    draw.background().color(PLUM);
 
-    let circle = square.below(square).shift_y(-pad);
-    draw.ellipse().xy(circle.xy()).wh(circle.wh()).color(SALMON);
+    draw.ellipse().color(STEELBLUE);
 
     draw.to_frame(app, &frame).unwrap();
 }
