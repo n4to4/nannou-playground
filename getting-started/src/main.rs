fbe7ca72c5923332b9b9c42cedf841fe6b4b3fbf
n4to4 <n4to4k@gmail.com>
2022-04-01 15:51:26 +0900

    line

--- getting-started/src/main.rs
+++ getting-started/src/main.rs
@@ -9,15 +9,14 @@ fn view(app: &App, frame: Frame) {
 
     draw.background().color(PLUM);
 
-    let point1 = pt2(-10.0, -20.0);
-    let point2 = pt2(10.0, -30.0);
-    let point3 = pt2(15.0, 40.0);
+    let start_point = pt2(-30.0, -20.0);
+    let end_point = pt2(40.0, 40.0);
 
-    draw.tri()
-        .color(STEELBLUE)
-        .w(300.0)
-        .h(200.0)
-        .points(point1, point2, point3);
+    draw.line()
+        .start(start_point)
+        .end(end_point)
+        .weight(4.0)
+        .color(STEELBLUE);
 
     draw.to_frame(app, &frame).unwrap();
 }
