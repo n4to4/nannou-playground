7b34e5a3e09e5191cf1b10b12b4d6b905f09901e
n4to4 <n4to4k@gmail.com>
2022-04-03 09:33:03 +0900

    Moving a circle

--- getting-started/src/main.rs
+++ getting-started/src/main.rs
@@ -1,23 +1,20 @@
 use nannou::prelude::*;
 
+struct Model {}
+
 fn main() {
-    nannou::sketch(view).run();
+    nannou::app(model).event(event).simple_window(view).run();
 }
 
-fn view(app: &App, frame: Frame) {
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn event(_app: &App, _model: &mut Model, _event: Event) {}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw();
-
     draw.background().color(PLUM);
-
-    let radius = 150.0;
-    let points = (0..=360).step_by(45).map(|i| {
-        let radian = deg_to_rad(i as f32);
-        let x = radian.sin() * radius;
-        let y = radian.cos() * radius;
-        (pt2(x, y), STEELBLUE)
-    });
-
-    draw.polygon().points_colored(points);
-
+    draw.ellipse().color(STEELBLUE).x_y(0.0, 0.0);
     draw.to_frame(app, &frame).unwrap();
 }
